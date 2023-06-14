e2c6ff71ef6b1c92f8c93aa53e6d279dd5644ef8
RomualdAMEGBEDJI <amrromuald234@gmail.com>
2023-06-14 08:20:50 -1200

    app for administrative staff

--- main/PIL_3/Administration/apps.py
+++ main/PIL_3/Administration/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AdmnistrationConfig(AppConfig):
+class AdministrationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'admnistration'
+    name = 'Administration'
