bba5b6d141ab860313fb01a81881efa0ca6f6f2a
RomualdAMEGBEDJI <amrromuald234@gmail.com>
2023-06-14 20:59:48 -1200

    update

--- main/PIL_3/PIL_3/settings.py
+++ main/PIL_3/PIL_3/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Admnistration',
+    'Students',
+    'Teachers',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,13 @@ ROOT_URLCONF = 'PIL_3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,''),
+                os.path.join(BASE_DIR,'Teachers/templates'),
+                os.path.join(BASE_DIR,'Administration/templates'),
+                os.path.join(BASE_DIR,'Students/templates'),
+
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -65,6 +74,12 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'Administration/static'),
+    os.path.join(BASE_DIR,'Students/static'),
+    os.path.join(BASE_DIR,'Teachers/static'),
 ]
 
 WSGI_APPLICATION = 'PIL_3.wsgi.application'
