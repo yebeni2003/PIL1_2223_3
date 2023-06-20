90d64280af6ba1c3a87b40290cb19eb41a9ba3f4
david15tonon <82893290+david15tonon@users.noreply.github.com>
2023-06-20 13:11:31 +0100

    Update models.py

--- main/PIL_3/Administration/models.py
+++ main/PIL_3/Administration/models.py
@@ -4,3 +4,12 @@ class Administrator(models.Model):
     name = models.CharField(max_length=20)
     email = models.EmailField(max_length=50)
     password = models.CharField( max_length=100) 
+
+class Emploi(models.Model):
+    cours = models.CharField(max_length=200)
+    heure = models.CharField(max_length=5)
+    duree_heure = models.IntField()
+class AdministratorConnexion(models.Model):
+    etudiant = models.OneToOneField(Etudiant, on_delete=models.CASCADE)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=128)
