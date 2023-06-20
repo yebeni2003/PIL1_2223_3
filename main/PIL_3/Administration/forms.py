43bf0dae1f41dd745006d58d73d5a953b9bcd4e4
david15tonon <82893290+david15tonon@users.noreply.github.com>
2023-06-20 12:02:26 +0100

    Add files via upload

---
+++ main/PIL_3/Administration/forms.py
@@ -0,0 +1,14 @@
+from .models import User1 
+#formulaire d'inscription
+class signup(forms.ModelForm):
+    class Meta:
+        model = User1
+        fields = ['name','prenom','email','phone','age','password']
+        widgets = {
+            'name': forms.TextInput(attrs={'class','form-control'})
+            'prenom': forms.TextInput(attrs={'class','form-control'})
+            'email' : forms.EmailInput(attrs={'class','form-control'})
+            'phone': forms.PasswordInput(render_value=True, attrs={'class','form-control'})
+            'age' : forms.NumberInput(attrs={'class','form-control'})
+            'password' : forms.TextInput(attrs={'class','form-control'})
+        }
