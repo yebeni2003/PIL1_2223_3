792cd6a18f33af5b2b5035e24a79a595d73e9dab
david15tonon <82893290+david15tonon@users.noreply.github.com>
2023-06-20 12:00:57 +0100

    Update models.py

--- main/PIL_3/Administration/models.py
+++ main/PIL_3/Administration/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
 # Create your models here.
+class Administrator(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50)
+    password = models.CharField( max_length=100) 
