107867f364bba585e42ba9917bd8763a1276352a
RomualdAMEGBEDJI <amrromuald234@gmail.com>
2023-06-21 14:51:33 +0100

    email confirmation

--- main/PIL_3/PIL_3/settings.py
+++ main/PIL_3/PIL_3/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from .info import *
+
+EMAIL_USE_TLS = EMAIL_USE_TLS
+EMAIL_HOST = EMAIL_HOST
+EMAIL_HOST_USER = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+EMAIL_PORT = EMAIL_PORT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
