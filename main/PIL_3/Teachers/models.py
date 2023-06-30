e727294f4cb05185b5d7c229f473ca26adcd6ccd
david15tonon <82893290+david15tonon@users.noreply.github.com>
2023-06-30 15:20:54 +0100

    Update models.py

--- main/PIL_3/Teachers/models.py
+++ main/PIL_3/Teachers/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import Teachers
+
 
 # Create your models here.
+
+class Teachers(models.Model):
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    email = models.EmailField()
+    # Autres champs
+
+
+class TeachersConnexion(models.Model):
+    Teachers = models.OneToOneField(Etudiant, on_delete=models.CASCADE)
+    username = models.CharField(max_length=50)
+    password = models.CharField
