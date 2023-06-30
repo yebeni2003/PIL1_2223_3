54829a8b3d7b3db9c9177308cdf077abba796af7
RomualdAMEGBEDJI <amrromuald234@gmail.com>
2023-06-30 03:21:43 -1200

    update urls.py

--- main/PIL_3/PIL_3/urls.py
+++ main/PIL_3/PIL_3/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('',include('Students.urls')),
     path('',include('Administration.urls')),
     path('reset_password',views.PasswordResetView.as_view(template_name='password_reset.html'),name='reset_password'),
-    path('reset_password_send',views.PasswordResetDoneView.as_view(),name='password_reset_done'),
-    path('reset/<uidb64>/<token>',views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
-    path('reset_password_complete',views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+    path('reset_password_send',views.PasswordResetDoneView.as_view(template_name='password_reset.sent.html'),name='password_reset_done'),
+    path('reset/<uidb64>/<token>',views.PasswordResetConfirmView.as_view(template_name='password_reset_form.html'),name="password_reset_confirm"),
+    path('reset_password_complete',views.PasswordResetCompleteView.as_view(template_name='password_reset_done.html'),name='password_reset_complete'),
 ]
